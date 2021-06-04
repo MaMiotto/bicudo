abd395d8df79fa331e0705dbd22f714a082bb32c
Matheus Veloso <49028621+MaMiotto@users.noreply.github.com>
2021-06-03 23:06:02 -0300

    Criando Rotas para as páginas de Login e Cadastro

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,11 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 
 import Home from '../pages/HomePage/Home'
+import Login from '../pages/LoginPage/login'
+import Cadastro from '../pages/CadastroPage/cadastro'
 
 function Routes(){
     return (
     <Switch>
         <Route path='/' component={Home} exact/>
+        <Route path='/login' component={Login} exact/>
+        <Route path='/cadastro' component={Cadastro} exact/>
     </Switch>
     )
 }
