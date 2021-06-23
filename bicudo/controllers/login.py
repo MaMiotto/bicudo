5339508c0e7e9d5b74bf925bf85e628d9f4e2ec8
Ricardo Bruno <ricardobrunoc@yahoo.com.br>
2021-06-23 14:13:36 -0300

    Upload do login

---
+++ bicudo/controllers/login.py
@@ -0,0 +1,46 @@
+def cors_origin():
+    origin = request.env.http_origin
+    headers = {}
+    headers['Access-Control-Allow-Origin'] = origin
+
+    headers['Access-Control-Allow-Methods'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Allow-Credentials'] = 'true';
+    response.headers.update(headers)
+
+    if request.env.request_method == 'OPTIONS':
+        headers['Content-Type'] = None
+        raise HTTP(200, '', **headers)
+
+def cors_allow(action):
+
+    def f(*args, **kwargs):
+        cors_origin()
+        return action(*args, **kwargs)
+
+    f.__doc__ = action.__doc__
+    f.__name__ = action.__name__
+    f.__dict__.update(action.__dict__)
+
+    return f
+
+@cors_allow
+@request.restful()
+def api():
+
+    def POST(*args, **vars):
+
+        email = vars["email"];
+        senha = vars["senha"];
+
+        usuario_existe = db(db.auth_user.email==email).select().first()
+
+        if(usuario_existe):
+            senha_correta = usuario_existe["password"] == senha
+            if(senha_correta):
+                return  response.json({"msg":"Login Válido!"})
+            else:
+                return response.json({"erro":"Credenciais Inválidas!"})
+        return response.json({"erro":"Credenciais Inválidas!"})
+        
+    return locals();
